9521e966a07aa8a07750a213a958442c79d67870
Cédric Baumann <cedric.baumann94@gmail.com>
2025-01-19 12:06:40 +0100

    Implement authorization with guards and dynamic error page handling for 403/404

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -10,12 +10,16 @@ export class ErrorPageComponent {
   errorCode: string = '404';
   errorMessage: string = 'Seite nicht gefunden';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.data.subscribe((data) => {
       if (data['errorCode']) {
         this.errorCode = data['errorCode'];
         this.errorMessage = data['errorMessage'] || this.errorMessage;
       }
     });
+  }
+
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
   }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import  { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -8,13 +9,49 @@ import { Observable } from 'rxjs';
 export class AuthService {
   private apiUrl = 'https://294.cyrotech.ch/users';
 
+  constructor(private http: HttpClient) {}
+
   register(userData: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/register-without-admin`, userData);
   }
 
-  constructor(private http: HttpClient) {}
-
   login(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  getDecodedToken(): any {
+    const token = this.getToken();
+    if (!token) return null;
+
+    try {
+      return jwtDecode(token);
+    } catch (error) {
+      console.error('Fehler beim Decodieren des Tokens:', error);
+      return null;
+    }
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  isAdmin(): boolean {
+    const decodedToken = this.getDecodedToken();
+    if (!decodedToken || !decodedToken.roles) {
+      return false;     }
+
+    return decodedToken.roles.includes('admin');
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
   }
 }
