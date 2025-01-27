9c60f7c062119177d070271737d5fe97bd553fde
Cédric Baumann <cedric.baumann94@gmail.com>
2025-01-27 18:10:29 +0100

    updated titles & icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
+import {Router, ActivatedRoute, NavigationEnd, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './shared/navbar/navbar.component';
 
 @Component({
@@ -8,4 +10,31 @@ import {NavbarComponent} from './shared/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route.snapshot.data['title'];
+        })
+      )
+      .subscribe((pageTitle: string) => {
+        if (pageTitle) {
+          this.titleService.setTitle(pageTitle);
+        } else {
+          this.titleService.setTitle('SuperDuperShop'); // Standard-Titel
+        }
+      });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,42 +11,109 @@ export const appRoutes: Routes = [
   {
     path: 'users',
     children: [
-      { path: 'login', loadComponent: () => import('./users/login/login.component').then(m => m.LoginComponent) },
-      { path: 'register', loadComponent: () => import('./users/register/register.component').then(m => m.RegisterComponent) },
-      { path: '', loadComponent: () => import('./users/user-list/user-list.component').then(m => m.UserListComponent), canActivate: [AdminGuard] },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./users/login/login.component').then((m) => m.LoginComponent),
+        data: { title: 'Benutzer Login' },
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./users/register/register.component').then((m) => m.RegisterComponent),
+        data: { title: 'Registrieren' },
+      },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./users/user-list/user-list.component').then((m) => m.UserListComponent),
+        canActivate: [AdminGuard],
+        data: { title: 'Benutzerliste' },
+      },
     ],
   },
 
   {
     path: 'products',
     children: [
-      { path: '', loadComponent: () => import('./products/product-list/product-list.component').then(m => m.ProductListComponent) },
-      { path: 'create', loadComponent: () => import('./products/product-create/product-create.component').then(m => m.ProductCreateComponent), canActivate: [AdminGuard] },
-      { path: ':id', loadComponent: () => import('./products/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
-      { path: 'edit/:id', loadComponent: () => import('./products/product-edit/product-edit.component').then(m => m.ProductEditComponent), canActivate: [AdminGuard] },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./products/product-list/product-list.component').then((m) => m.ProductListComponent),
+        data: { title: 'WhatsSuperDuperShop' },
+      },
+      {
+        path: 'create',
+        loadComponent: () =>
+          import('./products/product-create/product-create.component').then((m) => m.ProductCreateComponent),
+        canActivate: [AdminGuard],
+        data: { title: 'Produkt erstellen' },
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./products/product-detail/product-detail.component').then((m) => m.ProductDetailComponent),
+        data: { title: 'Produktdetails' },
+      },
+      {
+        path: 'edit/:id',
+        loadComponent: () =>
+          import('./products/product-edit/product-edit.component').then((m) => m.ProductEditComponent),
+        canActivate: [AdminGuard],
+        data: { title:  'Produkt bearbeiten' },
+      },
     ],
   },
 
   {
     path: 'categories',
     children: [
-      { path: '', loadComponent: () => import('./categories/category-list/category-list.component').then(m => m.CategoryListComponent) },
-      { path: 'create', loadComponent: () => import('./categories/category-create/category-create.component').then(m => m.CategoryCreateComponent), canActivate: [AdminGuard] },
-      { path: ':id', loadComponent: () => import('./categories/category-detail/category-detail.component').then(m => m.CategoryDetailComponent) },
-      { path: ':id/products', loadComponent: () => import('./products/product-list/product-list.component').then(m => m.ProductListComponent) },
-      { path: 'edit/:id', loadComponent: () => import('./categories/category-edit/category-edit.component').then(m => m.CategoryEditComponent), canActivate: [AdminGuard] },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./categories/category-list/category-list.component').then((m) => m.CategoryListComponent),
+        data: { title: 'Kategorien' },
+      },
+      {
+        path: 'create',
+        loadComponent: () =>
+          import('./categories/category-create/category-create.component').then((m) => m.CategoryCreateComponent),
+        canActivate: [AdminGuard],
+        data: { title: 'Kategorie erstellen' },
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./categories/category-detail/category-detail.component').then((m) => m.CategoryDetailComponent),
+        data: { title: 'Kategorie-Details' },
+      },
+      {
+        path: ':id/products',
+        loadComponent: () =>
+          import('./products/product-list/product-list.component').then((m) => m.ProductListComponent),
+        data: { title: 'Produkte in Kategorie' },
+      },
+      {
+        path: 'edit/:id',
+        loadComponent: () =>
+          import('./categories/category-edit/category-edit.component').then((m) => m.CategoryEditComponent),
+        canActivate: [AdminGuard],
+        data: { title: 'Kategorie bearbeiten' },
+      },
     ],
   },
 
   {
     path: '403',
-    loadComponent: () => import('./error-page/error-page.component').then(m => m.ErrorPageComponent),
-    data: { errorCode: '403', errorMessage: 'Keine Berechtigung auf dieser Seite' },
+    loadComponent: () =>
+      import('./error-page/error-page.component').then((m) => m.ErrorPageComponent),
+    data: { title: 'Zugriff verweigert', errorCode: '403', errorMessage: 'Keine Berechtigung auf dieser Seite' },
   },
   {
     path: '**',
-    loadComponent: () => import('./error-page/error-page.component').then(m => m.ErrorPageComponent),
-    data: { errorCode: '404', errorMessage: 'Seite nicht gefunden' },
+    loadComponent: () =>
+      import('./error-page/error-page.component').then((m) => m.ErrorPageComponent),
+    data: { title: 'Seite nicht gefunden', errorCode: '404', errorMessage: 'Seite nicht gefunden' },
   },
 ];
 
